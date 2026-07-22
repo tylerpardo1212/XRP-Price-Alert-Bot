c44c9a46c42ec42c5810247786b4157f9d4ff76a
tylerpardo1212 <tylerpardo1212@gmail.com>
2026-07-22 17:05:44 -0400

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -21,7 +21,7 @@ import base64
 # ---------- SETTINGS: edit these to match your grid bot ----------
 LOWER_LIMIT = 1.134            # your bot's lower limit (USD)
 UPPER_LIMIT = 1.164            # your bot's upper limit (USD)
-BUFFER_PCT = 0.25             # alert when price is within this % of the range from either edge
+BUFFER_PCT = 0.0             # alert when price is within this % of the range from either edge
 COOLDOWN_HOURS = .5          # don't re-alert for the same edge within this many hours
 # --------------------------------------------------------------------
 
