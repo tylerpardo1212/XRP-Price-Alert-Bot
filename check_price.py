92bf0b8cd4b9803b92573b8221ca093bd5b0908d
tylerpardo1212 <tylerpardo1212@gmail.com>
2026-07-23 11:45:52 -0400

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -22,7 +22,7 @@ import base64
 LOWER_LIMIT = 1.099            # your bot's lower limit (USD)
 UPPER_LIMIT = 1.1155            # your bot's upper limit (USD)
 BUFFER_PCT = 0.0             # alert when price is within this % of the range from either edge
-COOLDOWN_HOURS = 6          # don't re-alert for the same edge within this many hours
+COOLDOWN_HOURS = .5          # don't re-alert for the same edge within this many hours
 # --------------------------------------------------------------------
 
 STATE_FILE = "state.json"
